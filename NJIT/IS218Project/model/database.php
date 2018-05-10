b90e5bd5d37ade3e50f36059fd78e6f2c7ba1a9b
kharee e owens <keo6@afsaccess1.njit.edu>
2018-05-10 19:48:23 -0400

    created connection to the database

--- NJIT/IS218Project/model/database.php
+++ NJIT/IS218Project/model/database.php
@@ -1,27 +1,13 @@
 <?php
-
-{
-    private static $username = 'keo6';
-    private static $password = 'gntCaHLz';
-    private static $dsn = 'mysql:host=sql2.njit.edu;dbname=keo6';
-    private static $db;
-    private function __construct() {}
-    public static function getDB() {
-        if (!isset(self::$db)) { 
-            try {
-                self::$db = new PDO(self::$dsn, self::$username, self::$password);
-                echo '<h2>Connected to the database successfully!</h2>';
-            } catch (PDOException $error) {
-                echo '<h3>Database Error</h3>';
-                echo $error->getMessage().'<br>';
-                exit();
-            } catch (Exception $e) {
-                echo '<h3>Generic Error</h3>';
-                echo $e->getMessage().'<br>';
-                exit();
-            }
-        } 
-        return self::$db;
+    $hostname = "sql2.njit.edu" ; 
+	  $username = "keo6" ;
+	  $password = "gntCaHLz";
+	  $dsn = "mysql:host=$hostname;dbname=$username";
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
+        exit();
     }
-}
 ?>
